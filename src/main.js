fab7641dbba705686e02c24467417f56b3538714
Jeff <horusofoz@gmail.com>
2021-12-14 15:53:51 +1030

    Refactoring UI
    * Removed Portent panel
    * Set getPortent to fire from Portent menu button
    * Removed Twene panel
    * Set getTwene to fire from Twene menu button
    * Removed Write button
    * Made Oracle panel toggle display from Oracle button
    * Renamed some classes and variables for clearer meaning

--- src/main.js
+++ src/main.js
@@ -1,41 +1,25 @@
-const menuBtnWrite = document.querySelector("#menu-button-write");
 const menuBtnOracle = document.querySelector("#menu-button-oracle");
 const menuBtnPortent = document.querySelector("#menu-button-portent");
 const menuBtnTwene = document.querySelector("#menu-button-twene");
 
-const inputPanels = document.querySelectorAll(".panel-input");
-const inputPanelWrite = document.querySelector("#panel-input-write");
-const inputPanelOracle = document.querySelector("#panel-input-oracle");
-const inputPanelPortent = document.querySelector("#panel-input-portent");
-const inputPanelTwene = document.querySelector("#panel-input-twene");
+const panels = document.querySelectorAll(".panel");
+const panelOracle = document.querySelector("#panel-oracle");
 
-const oracleQuestionInput = document.querySelector("#input-question");
+const oracleInputQuestion = document.querySelector("#input-question");
 const oracleInputLikelihood = document.querySelector("#input-likelihood");
 
 /* UI */
-menuBtnWrite.addEventListener("click", function () {
-  setInputPanelDisplay();
-});
-
-menuBtnOracle.addEventListener("click", function () {
-  setInputPanelDisplay(inputPanelOracle);
-});
-
-menuBtnPortent.addEventListener("click", function () {
-  setInputPanelDisplay(inputPanelPortent);
-});
-
-menuBtnTwene.addEventListener("click", function () {
-  setInputPanelDisplay(inputPanelTwene);
-});
-
-function setInputPanelDisplay(displayInputPanel) {
-  inputPanels.forEach(function (inputPanel) {
-    inputPanel.classList.add("hidden");
-  });
-
-  if (displayInputPanel !== undefined) {
-    displayInputPanel.classList.remove("hidden");
+function setPanelVisibility(panel) {
+  // If panel already visible, hide it
+  if (!panel.classList.contains("hidden")) {
+    panel.classList.add("hidden");
+  }
+  // else hide all panels then display input panel
+  else {
+    panels.forEach(function (panel) {
+      panel.classList.add("hidden");
+    });
+    panel.classList.remove("hidden");
   }
 }
 
@@ -48,11 +32,14 @@ function getRandomNum(min, max) {
 
 /*ORACLE*/
 const btnOracleGet = document.querySelector("#oracle-get");
-
 let oracleLikelihood = "even";
 
+menuBtnOracle.addEventListener("click", function () {
+  setPanelVisibility(panelOracle);
+});
+
 btnOracleGet.addEventListener("click", function () {
-  if (oracleQuestionInput.value === "") {
+  if (oracleInputQuestion.value === "") {
     tinyMCE.execCommand(
       "mceInsertContent",
       false,
@@ -67,21 +54,13 @@ btnOracleGet.addEventListener("click", function () {
   resetOracleInputs();
 });
 
-oracleQuestionInput.addEventListener("change", function () {
-  if (oracleQuestionInput.value.trim() === "") {
-    btnOracleGet.disabled = true;
-  } else {
-    btnOracleGet.disabled = false;
-  }
-});
-
 oracleInputLikelihood.addEventListener("change", function () {
   oracleLikelihood = oracleInputLikelihood.value;
 });
 
 function getOracleAnswer() {
   let oracle = {};
-  oracle.question = oracleQuestionInput.value;
+  oracle.question = oracleInputQuestion.value;
   oracle.likelihood = oracleLikelihood;
   oracle.roll1 = getRandomNum(1, 6);
   oracle.roll2 = getRandomNum(1, 6);
@@ -136,15 +115,14 @@ function setOracleOutput(string) {
 }
 
 function resetOracleInputs() {
-  oracleQuestionInput.value = "";
+  oracleInputQuestion.value = "";
   oracleInputLikelihood.value = "even";
   oracleLikelihood = "even";
 }
 
 /* PORTENT */
-const btnPortentGet = document.querySelector('#portent-get');
 
-btnPortentGet.addEventListener('click', function() {
+menuBtnPortent.addEventListener("click", function () {
   let portent = getPortent();
   let string = getPortentString(portent);
   setPortentOutput(string);
@@ -171,14 +149,376 @@ function setPortentOutput(string) {
   tinyMCE.execCommand("mceInsertContent", false, string);
 }
 
-const verbs = ['rearrange', 'trap', 'unseal', 'isolate', 'avoid', 'corrupt', 'communicate', 'sneak', 'bestow', 'interrupt', 'restore', 'exhaust', 'befriend', 'curse', 'create', 'find', 'deactivate', 'detect', 'move', 'deny', 'punish', 'regenerate', 'attempt', 'assuage', 'disturb', 'decrease', 'abjur', 'injure', 'fight', 'foil', 'shatter', 'ambnush', 'take', 'unearth', 'eliminate', 'inflict', 'reveal', 'destroy', 'warn', 'poison', 'decimate', 'discourage', 'merge', 'follow', 'pardon', 'observe', 'entertain', 'destabilize', 'succeed', 'trespass', 'exchange', 'raise', 'heal', 'lose', 'convince', 'improve', 'humiliate', 'guide', 'divide', 'identify', 'join', 'condemn', 'grow', 'absolve', 'violate', 'enslave', 'strengthen', 'seal', 'trick', 'deviate', 'reward', 'cleanse', 'defeat', 'assist', 'hide', 'delay', 'execute', 'release', 'allow', 'alter', 'silence', 'replace', 'learn', 'bless', 'bargain', 'prevent', 'weaken', 'beguilde', 'pacify', 'forget', 'control', 'degrade', 'fail', 'defenestrate', 'judge', 'antagonize', 'distract', 'halt', 'discover', 'avenge', 'gather', 'imitate', 'fix', 'discuss', 'return', 'plan', 'explore', 'refresh', 'begin', 'travel', 'clarify', 'mesmerize', 'wish', 'confuse', 'meet', 'spoil', 'steal', 'ponder', 'confront', 'remember'];
-const adjectives = ['helpful', 'graceful', 'maniacal', 'slovenly', 'terrible', 'curious', 'ironic', 'amusing', 'metaphorical', 'material', 'physical', 'advanced', 'tragic', 'profitable', 'empowered', 'hopeless', 'amicable', 'treacherous', 'artificial', 'active', 'defiant', 'despondent', 'timid', 'theoretical', 'precise', 'scientific', 'intact', 'brazen', 'painful', 'fallen', 'recent', 'delicious', 'diruptive', 'hallucinatory', 'traumatic', 'wonderous', 'cautious', 'common', 'nearby', 'haphazard', 'infinite', 'unholy', 'obvious', 'pandering', 'benevolent', 'rare', 'momentary', 'fetid', 'simple', 'inconvenient', 'religious', 'permanent', 'holy', 'erratic', 'complex', 'standard', 'opportune', 'deceptive', 'orderly', 'malevolent', 'bewildering', 'infamous', 'uncertain', 'psychological', 'hostile', 'personal', 'disappointing', 'humble', 'ghostly', 'immaculate', 'frightful', 'awkward', 'gaudy', 'occult', 'ambitious', 'tender', 'unfair', 'unlikely', 'angelic', 'diabolical', 'heroic', 'fraudulent', 'eldritch', 'stealthy', 'arrogant', 'broken', 'impending', 'restrained', 'mental', 'minor', 'artistic', 'drained', 'prodigious', 'sacreligious', 'hopeful', 'ancient', 'elusive', 'eternal', 'widespread', 'feral', 'new', 'bygone', 'distant', 'medical', 'irksome', 'fundamental', 'aberrant', 'callous', 'dorman', 'unclear', 'disastrous', 'famous', 'temporary', 'arcane', 'impressive', 'aggressive', 'fair', 'spiritual', 'vague', 'repulsive'];
-const nouns = ['solution', 'pandemic', 'student', 'work', 'contraband', 'mystery', 'evidence', 'advice', 'temple', 'tool', 'foe', 'mother', 'truth', 'scheme', 'journey', 'dispute', 'death', 'deity', 'time', 'dream', 'illness', 'superstition', 'lies', 'despair', 'barrier', 'treasure', 'servant', 'foreigner', 'soul', 'network', 'health', 'reputation', 'poverty', 'conflict', 'purity', 'companion', 'captivity', 'government', 'monster', 'beast', 'crime', 'creature', 'vault', 'affliction', 'daemon', 'murder', 'portal', 'sustenance', 'goal', 'laboratory', 'fear', 'emotion', 'romance', 'technology', 'merchant', 'prison', 'secret', 'flame', 'council', 'child', 'prophecy', 'peace', 'path', 'civilization', 'exposure', 'chaos', 'food', 'obstacle', 'grudge', 'vehicle', 'source', 'belief', 'defense', 'love', 'structure', 'game', 'resource', 'wreckage', 'trial', 'trap', 'scrum', 'wilderness', 'mentor', 'father', 'aura', 'tension', 'rebellion', 'lookout', 'font', 'knowledge', 'evil', 'freedom', 'relief', 'investment', 'haven', 'relative', 'locals', 'underling', 'weapon', 'pain', 'tourist', 'agreement', 'rage', 'goodness', 'skill', 'hive', 'ability', 'salvation', 'nightmare', 'training', 'estate', 'team', 'leader', 'container', 'magic', 'puzzle', 'monument', 'wealth', 'resurrection', 'stockpile'];
+const verbs = [
+  "rearrange",
+  "trap",
+  "unseal",
+  "isolate",
+  "avoid",
+  "corrupt",
+  "communicate",
+  "sneak",
+  "bestow",
+  "interrupt",
+  "restore",
+  "exhaust",
+  "befriend",
+  "curse",
+  "create",
+  "find",
+  "deactivate",
+  "detect",
+  "move",
+  "deny",
+  "punish",
+  "regenerate",
+  "attempt",
+  "assuage",
+  "disturb",
+  "decrease",
+  "abjur",
+  "injure",
+  "fight",
+  "foil",
+  "shatter",
+  "ambnush",
+  "take",
+  "unearth",
+  "eliminate",
+  "inflict",
+  "reveal",
+  "destroy",
+  "warn",
+  "poison",
+  "decimate",
+  "discourage",
+  "merge",
+  "follow",
+  "pardon",
+  "observe",
+  "entertain",
+  "destabilize",
+  "succeed",
+  "trespass",
+  "exchange",
+  "raise",
+  "heal",
+  "lose",
+  "convince",
+  "improve",
+  "humiliate",
+  "guide",
+  "divide",
+  "identify",
+  "join",
+  "condemn",
+  "grow",
+  "absolve",
+  "violate",
+  "enslave",
+  "strengthen",
+  "seal",
+  "trick",
+  "deviate",
+  "reward",
+  "cleanse",
+  "defeat",
+  "assist",
+  "hide",
+  "delay",
+  "execute",
+  "release",
+  "allow",
+  "alter",
+  "silence",
+  "replace",
+  "learn",
+  "bless",
+  "bargain",
+  "prevent",
+  "weaken",
+  "beguilde",
+  "pacify",
+  "forget",
+  "control",
+  "degrade",
+  "fail",
+  "defenestrate",
+  "judge",
+  "antagonize",
+  "distract",
+  "halt",
+  "discover",
+  "avenge",
+  "gather",
+  "imitate",
+  "fix",
+  "discuss",
+  "return",
+  "plan",
+  "explore",
+  "refresh",
+  "begin",
+  "travel",
+  "clarify",
+  "mesmerize",
+  "wish",
+  "confuse",
+  "meet",
+  "spoil",
+  "steal",
+  "ponder",
+  "confront",
+  "remember",
+];
+const adjectives = [
+  "helpful",
+  "graceful",
+  "maniacal",
+  "slovenly",
+  "terrible",
+  "curious",
+  "ironic",
+  "amusing",
+  "metaphorical",
+  "material",
+  "physical",
+  "advanced",
+  "tragic",
+  "profitable",
+  "empowered",
+  "hopeless",
+  "amicable",
+  "treacherous",
+  "artificial",
+  "active",
+  "defiant",
+  "despondent",
+  "timid",
+  "theoretical",
+  "precise",
+  "scientific",
+  "intact",
+  "brazen",
+  "painful",
+  "fallen",
+  "recent",
+  "delicious",
+  "diruptive",
+  "hallucinatory",
+  "traumatic",
+  "wonderous",
+  "cautious",
+  "common",
+  "nearby",
+  "haphazard",
+  "infinite",
+  "unholy",
+  "obvious",
+  "pandering",
+  "benevolent",
+  "rare",
+  "momentary",
+  "fetid",
+  "simple",
+  "inconvenient",
+  "religious",
+  "permanent",
+  "holy",
+  "erratic",
+  "complex",
+  "standard",
+  "opportune",
+  "deceptive",
+  "orderly",
+  "malevolent",
+  "bewildering",
+  "infamous",
+  "uncertain",
+  "psychological",
+  "hostile",
+  "personal",
+  "disappointing",
+  "humble",
+  "ghostly",
+  "immaculate",
+  "frightful",
+  "awkward",
+  "gaudy",
+  "occult",
+  "ambitious",
+  "tender",
+  "unfair",
+  "unlikely",
+  "angelic",
+  "diabolical",
+  "heroic",
+  "fraudulent",
+  "eldritch",
+  "stealthy",
+  "arrogant",
+  "broken",
+  "impending",
+  "restrained",
+  "mental",
+  "minor",
+  "artistic",
+  "drained",
+  "prodigious",
+  "sacreligious",
+  "hopeful",
+  "ancient",
+  "elusive",
+  "eternal",
+  "widespread",
+  "feral",
+  "new",
+  "bygone",
+  "distant",
+  "medical",
+  "irksome",
+  "fundamental",
+  "aberrant",
+  "callous",
+  "dorman",
+  "unclear",
+  "disastrous",
+  "famous",
+  "temporary",
+  "arcane",
+  "impressive",
+  "aggressive",
+  "fair",
+  "spiritual",
+  "vague",
+  "repulsive",
+];
+const nouns = [
+  "solution",
+  "pandemic",
+  "student",
+  "work",
+  "contraband",
+  "mystery",
+  "evidence",
+  "advice",
+  "temple",
+  "tool",
+  "foe",
+  "mother",
+  "truth",
+  "scheme",
+  "journey",
+  "dispute",
+  "death",
+  "deity",
+  "time",
+  "dream",
+  "illness",
+  "superstition",
+  "lies",
+  "despair",
+  "barrier",
+  "treasure",
+  "servant",
+  "foreigner",
+  "soul",
+  "network",
+  "health",
+  "reputation",
+  "poverty",
+  "conflict",
+  "purity",
+  "companion",
+  "captivity",
+  "government",
+  "monster",
+  "beast",
+  "crime",
+  "creature",
+  "vault",
+  "affliction",
+  "daemon",
+  "murder",
+  "portal",
+  "sustenance",
+  "goal",
+  "laboratory",
+  "fear",
+  "emotion",
+  "romance",
+  "technology",
+  "merchant",
+  "prison",
+  "secret",
+  "flame",
+  "council",
+  "child",
+  "prophecy",
+  "peace",
+  "path",
+  "civilization",
+  "exposure",
+  "chaos",
+  "food",
+  "obstacle",
+  "grudge",
+  "vehicle",
+  "source",
+  "belief",
+  "defense",
+  "love",
+  "structure",
+  "game",
+  "resource",
+  "wreckage",
+  "trial",
+  "trap",
+  "scrum",
+  "wilderness",
+  "mentor",
+  "father",
+  "aura",
+  "tension",
+  "rebellion",
+  "lookout",
+  "font",
+  "knowledge",
+  "evil",
+  "freedom",
+  "relief",
+  "investment",
+  "haven",
+  "relative",
+  "locals",
+  "underling",
+  "weapon",
+  "pain",
+  "tourist",
+  "agreement",
+  "rage",
+  "goodness",
+  "skill",
+  "hive",
+  "ability",
+  "salvation",
+  "nightmare",
+  "training",
+  "estate",
+  "team",
+  "leader",
+  "container",
+  "magic",
+  "puzzle",
+  "monument",
+  "wealth",
+  "resurrection",
+  "stockpile",
+];
 
 /* TWENE */
-const btnTweneGet = document.querySelector('#twene-get');
 
-btnTweneGet.addEventListener('click', function() {
+menuBtnTwene.addEventListener("click", function () {
   let twene = getTwene();
   let string = getTweneString(twene);
   setTweneOutput(string);
